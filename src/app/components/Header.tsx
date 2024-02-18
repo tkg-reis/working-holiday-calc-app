fcba3cb886bbeb043d6411f0fcd955f5c1909600
tkg-reis <oorgns1uw5sk3ae@gmail.com>
2024-02-18 23:54:29 +0900

    fix UI and create launch.json

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className='p-4'>
-        <h1>ワーキングホリデー充実度計算アプリ</h1>
+    <header className="p-4 ">
+      <h1>ワーキングホリデー充実度計算アプリ</h1>
+      <ul className="flex justify-center items-center">
+        <li className="mx-3">
+          <Link href="/about">about</Link>
+        </li>
+        <li className="mx-3">
+          <Link href="/opinion">opinion</Link>
+        </li>
+        <li className="mx-3">
+          <Link href="/article">article</Link>
+        </li>
+        <li className="mx-3">
+          <Link href="/news">news</Link>
+        </li>
+      </ul>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
---
+++ src/app/opinion/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Opinion = () => {
+  return (
+    <div>Opinion</div>
+  )
+}
+
+export default Opinion
