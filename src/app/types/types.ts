3cf336d9be1a3426e6a5a3eebff1b86e61fb4a47
tkg-reis <oorgns1uw5sk3ae@gmail.com>
2024-04-27 18:22:27 +0900

    型定義ファイルの更新などを更新しました

--- src/app/types/types.ts
+++ src/app/types/types.ts
@@ -3,3 +3,17 @@ export type OptionType = {
     readonly value: string;
     readonly label: string;
 };
+
+export interface dataType {
+    id: number;
+    your_age: number;
+    travel_money: number;
+    insurance : number;
+    english_level : number;
+    stay_time : number;
+    country : string;
+    cook : number;
+    parttime_work : number;
+    language_school : number;
+    frequency_travel : number;
+}
